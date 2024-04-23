dfbaae163ca12ddedb31b23e340cc0b4a28ff5d4
Лёша <lgmkfgdl@gmail.com>
2024-04-23 09:20:57 +0900

    first commit

---
+++ Model/Role.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лб4.Model
+{
+    public class Role
+    {
+        public int Id { get; set; }
+        public string NameRole { get; set; }
+        public Role() { }
+        public Role(int id, string nameRole)
+        {
+            this.Id = id;
+            this.NameRole = nameRole;
+        }
+    }
+}
+
